72395a41a57f4a7b4d9b11bc67aa07c90debf697
TrueStrikes <137821671+TrueStrikes@users.noreply.github.com>
2023-07-29 08:21:05 -0400

    Update Opener.py

--- Opener.py
+++ Opener.py
@@ -1,23 +1,47 @@
-import discord
+import requests
+import json
 import subprocess
+import time
 
-intents = discord.Intents.default()
-intents.message_content = True
+# Change the browser path here as needed
+BROWSER_PATH = r'YOURBROWSERPATH'
 
-client = discord.Client(intents=intents)
+# Set to keep track of opened links
+opened_links = set()
 
-@client.event
-async def on_ready():
-    print('Watching your channel for links.')
+def open_in_browser(url):
+    subprocess.run([BROWSER_PATH, url], shell=True)
 
-@client.event
-async def on_message(message):
-    if message.channel.id == your_channel_id:
-        if len(message.embeds) > 0:
-            for embed in message.embeds:
-                if embed.url:
-                    link = embed.url
-                    browser = r"your_broswer_executable_path"
-                    subprocess.run([browser, link], shell=True)
+def retrieve_latest_message(channelid):
+    headers = {
+        'authorization': 'yourtoken'
+    }
+    r = requests.get(f'https://discord.com/api/v8/channels/{channelid}/messages', headers=headers)
+    messages = json.loads(r.text)
+    
+    if not isinstance(messages, list) or len(messages) == 0:
+        print("No messages found in the channel.")
+        return
 
-client.run('Yourbottoken')
+    latest_message = messages[0]  # The latest message is the first in the list
+    content = latest_message.get('content')
+    if content:
+        print(f"Message Content: {content}")
+
+    embeds = latest_message.get('embeds')
+    if embeds:
+        print("Embeds:")
+        for embed in embeds:
+            print(embed, '\n')
+            roblox_url = embed.get('url')
+            if roblox_url and 'roblox.com/catalog/' in roblox_url and roblox_url not in opened_links:
+                opened_links.add(roblox_url)  # Add the link to the set
+                open_in_browser(roblox_url)
+
+# Change the channel ID here as needed
+channel_id = 'yourchannelid'
+
+# Set the loop to run indefinitely
+while True:
+    retrieve_latest_message(channel_id)
+    time.sleep(0.2)  # Add a wait time of 0.2 seconds before the next iteration
