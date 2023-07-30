77a70eb3aa1d5ef64fc22892fefbd4b58dbf973d
TrueStrikes <137821671+TrueStrikes@users.noreply.github.com>
2023-07-30 13:52:59 -0400

    Update opener.py

--- opener.py
+++ opener.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import webbrowser
+import os
 import time
 from colorama import init, Fore
 
@@ -9,9 +10,6 @@ init()
 
 # Set to keep track of opened links
 opened_links = set()
-
-# Replace 'YOUR_WEBHOOK_URL_HERE' with your actual webhook URL
-YOUR_WEBHOOK_URL = 'yourwebhook'
 
 def send_discord_webhook(webhook_url, message):
     data = {
@@ -24,31 +22,20 @@ def send_discord_webhook(webhook_url, message):
     if response.status_code != 204:
         print(f"Failed to send webhook. Status code: {response.status_code}")
 
-def retrieve_latest_message(channelid):
+def retrieve_latest_message(channel_id, token, webhook_url):
     headers = {
-        'authorization': 'yourtoken'
+        'authorization': token
     }
     params = {
         'limit': 1
     }
-    r = requests.get(f'https://discord.com/api/v8/channels/{channelid}/messages', headers=headers, params=params)
+    r = requests.get(f'https://discord.com/api/v8/channels/{channel_id}/messages', headers=headers, params=params)
     messages = json.loads(r.text)
     
     if not isinstance(messages, list) or len(messages) == 0:
         return
 
     latest_message = messages[0]  # The latest message is the first in the list
-
-    content = latest_message.get('content', '').lower()
-    if 'roblox.com/catalog/' in content:
-        roblox_links = find_roblox_links(content)
-        for link in roblox_links:
-            if link not in opened_links:
-                opened_links.add(link)  # Add the link to the set
-                message_to_send = "Opened a link: " + link
-                send_discord_webhook(YOUR_WEBHOOK_URL, message_to_send)
-                print(f"{Fore.RED}{message_to_send}{Fore.RESET}")
-                open_in_browser(link)
 
     embeds = latest_message.get('embeds')
     if embeds:
@@ -58,7 +45,7 @@ def retrieve_latest_message(channelid):
                 if roblox_url and roblox_url not in opened_links:
                     opened_links.add(roblox_url)  # Add the link to the set
                     message_to_send = "Opened a link: " + roblox_url
-                    send_discord_webhook(YOUR_WEBHOOK_URL, message_to_send)
+                    send_discord_webhook(webhook_url, message_to_send)
                     print(f"{Fore.RED}{message_to_send}{Fore.RESET}")
                     open_in_browser(roblox_url)
 
@@ -69,15 +56,39 @@ def find_roblox_links(text):
     import re
     return re.findall(r'https?://(?:www\.)?roblox\.com/catalog/\d+', text)
 
-# Change the channel ID here as needed
-channel_id = '1094291863332192376'
+def load_config():
+    try:
+        with open('config.json', 'r') as config_file:
+            config = json.load(config_file)
+            return config
+    except FileNotFoundError:
+        print("Config file 'config.json' not found.")
+        return None
+    except json.JSONDecodeError:
+        print("Invalid JSON format in 'config.json'.")
+        return None
 
-# Clear the console and print "The bot is working, there's just nothing to open" in yellow
-import os
-os.system('cls' if os.name == 'nt' else 'clear')
-print(f"{Fore.YELLOW}The bot is working, there's just nothing to open{Fore.RESET}")
+def main():
+    config = load_config()
+    if config is None:
+        return
 
-# Set the loop to run indefinitely with a 0.2 seconds wait
-while True:
-    retrieve_latest_message(channel_id)
-    time.sleep(0.2)
+    channel_id = config.get('channel_id')
+    webhook_url = config.get('webhook_url')
+    token = config.get('token')
+
+    if not channel_id or not webhook_url or not token:
+        print("Missing required variables in 'config.json'.")
+        return
+
+    # Clear the console and print "The bot is working, there's just nothing to open" in yellow
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(f"{Fore.YELLOW}The bot is working, there's just nothing to open{Fore.RESET}")
+
+    # Set the loop to run indefinitely with a 0.2 seconds wait
+    while True:
+        retrieve_latest_message(channel_id, token, webhook_url)
+        time.sleep(0.2)
+
+if __name__ == "__main__":
+    main()
